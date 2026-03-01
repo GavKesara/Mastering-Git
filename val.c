8550259ef3d1b82dcf9ab69da007ff535eb56964
Gavesh Dissanayake <dissanayakegavesh@gmail.com>
2026-03-01 21:01:38 +0530

    Updated Val text

--- val.c
+++ val.c
@@ -5,7 +5,7 @@ int main(void)
     int repeat;
     printf("How Strong is your love 1-10:\n");
     scanf("%d",&repeat);
-    printf("I love you\n very");
+    printf("I Dont love you\n very");
     while(repeat>0)
     {
         printf("\n very");
