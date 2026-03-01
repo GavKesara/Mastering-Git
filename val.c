fa9ce3f4344b3f370942e7efd0eaf93f141b1d94
Gavesh Dissanayake <dissanayakegavesh@gmail.com>
2026-03-01 20:55:38 +0530

    first Commit

---
+++ val.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int repeat;
+    printf("How Strong is your love 1-10:\n");
+    scanf("%d",&repeat);
+    printf("I love you\n very");
+    while(repeat>0)
+    {
+        printf("\n very");
+        repeat--;
+    };
+    printf(" much.\n\n");
+    return 0;
+}
